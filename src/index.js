8886eb278339a721568a317eb9b0ef7ce8439f94
koonukaame <marialezhebokova@yandex.ru>
2023-02-26 19:18:44 +0300

    feat: solve brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arr = str.split('');
+  
+  function search(arr) {
+    for (let x = 0; x < arr.length; x++) {
+    for (let y = 0; y < bracketsConfig.length; y++) {
+      if(arr[x] === bracketsConfig[y][0] && arr[x + 1] === bracketsConfig[y][1]) {
+        arr.splice(x, 2);
+        search(arr);
+      }
+    }
+  }
+  }
+
+search(arr);
+
+if (arr.length === 0) {
+  return true;
+}
+return false;
 }
